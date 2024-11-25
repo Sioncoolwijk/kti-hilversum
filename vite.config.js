c068a5f2285af9cf1608ea5006fcf3b6289e1eeb
Sion Coolwijk <sion.coolwijk@coolblue.nl>
2024-11-25 17:12:40 +0100

    update mixedmodules

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 });
